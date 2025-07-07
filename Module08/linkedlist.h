13b868b2822b57bc5d513d2c6d034550c3cea13f
blouyakis <bobbilouyakis@icloud.com>
2025-07-07 12:02:52 -0400

    lab 7.2

--- Module08/linkedlist.h
+++ Module08/linkedlist.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
 // TODO: Create your node_t type here
 typedef struct node
 {
@@ -58,19 +59,55 @@ void add_back(NeuList *list, int wins, int year) {
 
 void print_list(NeuList *list)
 {
-    // todo
+    Pair *curr = list->head;
+    while (curr != NULL)
+    {
+        printf("(%d, %d)", curr->num_wins, curr->year);
+        if (curr->next != NULL)
+        {
+            printf(" -> ");
+        }
+        curr = curr->next;
+    }
+    printf("\n");
 }
 
 Pair *find(NeuList *list, int year)
 {
-    // todo
-    return NULL;
+    // Check if list is NULL or empty
+    if (list == NULL || list->head == NULL) {
+        return NULL;
+    }
+    
+    // Traverse the list to find the node with matching year
+    Pair *curr = list->head;
+    while (curr != NULL) {
+        if (curr->year == year) {
+            return curr;  // Found the node
+        }
+        curr = curr->next;
+    }
+    
+    return NULL;  // Year not found in the list
 }
-
 
 void free_list(NeuList *list)
 {
-    // todo
+    // Check if list is NULL
+    if (list == NULL) {
+        return;
+    }
+    
+    // Free all nodes in the list
+    Pair *curr = list->head;
+    while (curr != NULL) {
+        Pair *temp = curr;      // Store current node
+        curr = curr->next;      // Move to next node
+        free(temp);             // Free the stored node
+    }
+    
+    // Free the list structure itself
+    free(list);
 }
 
 #endif //LINKED_LIST_H
